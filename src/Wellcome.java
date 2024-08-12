6c04afe4c9435281b351039b31cba2ed8fabedcb
dominhthuan2005 <dominhthuan17102005@gmail.com>
2024-08-12 14:07:28 +0700

    First Commit

---
+++ src/Wellcome.java
@@ -0,0 +1,6 @@
+public class Wellcome {
+    public static void main(String[] args) {
+        System.out.printf("Hello Thuan DO!\n");
+        System.out.printf("Welcome to the DHKTPM19B!");
+    }
+}
